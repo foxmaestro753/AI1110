c136b78a8d3df96dee6475cd43f9a83f7f95cad3
Sayan Biswas <foxmaestro@Mac.local>
2023-04-30 20:57:20 +0530

    twelth commit

---
+++ assingment1/Codes/assignment1.py
@@ -0,0 +1,18 @@
+from fractions import Fraction
+import math
+
+ball = int(input("Enter the number of balls : "))
+red = int(input("Enter the number of red balls : "))
+black = int(input("Enter the number of black balls : "))
+
+while ball != (red+black) :
+    red = int(input("Enter the number of red balls : "))
+    black = int(input("Enter the number of black balls : "))
+
+answer1 = (math.comb(red,1))*(math.comb(red,1))
+answer2 = (math.comb(black,1))*(math.comb(red,1))
+answer3 = (math.comb(red,1))*(math.comb(black,1)) + (math.comb(black,1))*(math.comb(red,1))
+
+count = (math.comb(ball,1))*(math.comb(ball,1))
+
+print(f"Question 1 answer = {Fraction(answer1,count)}\nQuestion 2 answer = {Fraction(answer2,count)}\nQuestion 3 answer = {Fraction(answer3,count)}\n")
