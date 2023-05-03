05d930a0284e88c5a6500ca3b06f1635164344f8
Sayan Biswas <foxmaestro@Mac.local>
2023-05-03 20:59:24 +0530

    sixteenth commit

--- assignment1/Codes/assignment1.py
+++ assignment1/Codes/assignment1.py
@@ -10,9 +10,8 @@ while ball != (red+black) :
     black = int(input("Enter the number of black balls : "))
 
 answer1 = (math.comb(red,1))*(math.comb(red,1))
-answer2 = (math.comb(black,1))*(math.comb(red,1))
-answer3 = (math.comb(red,1))*(math.comb(black,1)) + (math.comb(black,1))*(math.comb(red,1))
+answer2 = (math.comb(red,1))*(math.comb(black,1)) + (math.comb(black,1))*(math.comb(red,1))
 
 count = (math.comb(ball,1))*(math.comb(ball,1))
 
-print(f"Question 1 answer = {Fraction(answer1,count)}\nQuestion 2 answer = {Fraction(answer2,count)}\nQuestion 3 answer = {Fraction(answer3,count)}\n")
+print(f"Question 1 answer = {Fraction(answer1,count)}\nQuestion 2 answer = {Fraction(answer2,count)}\n")
