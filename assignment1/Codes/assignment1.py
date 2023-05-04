a7b9f1a928154f7bd6ad551007bdee9feeb6e0d7
Sayan Biswas <foxmaestro@Mac.local>
2023-05-04 19:11:25 +0530

    twentieth commit

--- assignment1/Codes/assignment1.py
+++ assignment1/Codes/assignment1.py
@@ -1,19 +1,18 @@
-# importing required libraries
-from fractions import Fraction
-from scipy.stats import binom
+import numpy as np
 import matplotlib.pyplot as plt
-# simulating random variable X
-# setting the values of n and p
-n = 2          #number of trails
+from scipy.stats import bernoulli
+from scipy.stats import norm
+from scipy.stats import binom
+#Simlen
+simlen=10000000
+#Number of hurdles
+n = 2
+#Probability of clearing a hurdle
 p='4/9'        #probability of success
 p=Fraction(p)  #change the type of x from string to Fraction
 p=float(p)     #change the type of x from Fraction to float
-# list of pmf values
-dist0 = [binom.pmf(0, n, p)]
-dist1 = [binom.pmf(1, n, p)]
-dist2 = [binom.pmf(2, n, p)]
-# plotting the graph
-plt.bar(0, dist0)
-plt.bar(1, dist1)
-plt.bar(2, dist2)
-plt.show()
+# Simulating the probability using the binomial random variable
+data_binom = binom.rvs(n,p,size=simlen) #Simulating the event of jumping 10 hurdles
+err_ind2, err_ind1 = np.nonzero(data_binom == 2), np.nonzero(data_binom == 1) #checking probability condition
+err_n2, err_n1 = np.size(err_ind2), np.size(err_ind1) #computing the probability
+print(f"P(X=2) = {err_n2/simlen}, P(X=1) = {err_n1/simlen}")
