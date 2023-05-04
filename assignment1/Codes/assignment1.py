d50e267c73c4287f5406aec4ad36d7efe256ec9a
Sayan Biswas <foxmaestro@Mac.local>
2023-05-04 14:26:41 +0530

    eighteenth commit

--- assignment1/Codes/assignment1.py
+++ assignment1/Codes/assignment1.py
@@ -1,17 +1,17 @@
+# importing required libraries
 from fractions import Fraction
-import math
-
-ball = int(input("Enter the number of balls : "))
-red = int(input("Enter the number of red balls : "))
-black = int(input("Enter the number of black balls : "))
-
-while ball != (red+black) :
-    red = int(input("Enter the number of red balls : "))
-    black = int(input("Enter the number of black balls : "))
-
-answer1 = (math.comb(red,1))*(math.comb(red,1))
-answer2 = (math.comb(red,1))*(math.comb(black,1)) + (math.comb(black,1))*(math.comb(red,1))
-
-count = (math.comb(ball,1))*(math.comb(ball,1))
-
-print(f"Question 1 answer = {Fraction(answer1,count)}\nQuestion 2 answer = {Fraction(answer2,count)}\n")
+from scipy.stats import binom
+import matplotlib.pyplot as plt
+# simulating random variable X
+# setting the values of n and p
+n = 2          #number of trails
+p='4/9'        #probability of success
+p=Fraction(p)  #change the type of x from string to Fraction
+p=float(p)     #change the type of x from Fraction to float
+# defining list of r values
+r_values = list(range(n + 1))
+# list of pmf values
+dist = [binom.pmf(r, n, p) for r in r_values ]
+# plotting the graph
+plt.bar(r_values, dist)
+plt.show()
